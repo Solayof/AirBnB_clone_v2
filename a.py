fcd620a58fc9ffed50d8648ac55b9eb0c1524ff7
Huclark <vhuclark@gmail.com>
2024-02-20 12:38:57 +0000

    Add: cache

--- a.py
+++ a.py
@@ -1,12 +1,21 @@
 class Ch():
-    city = "Hello world"
+    __state = "Hello world"
     
     @property
-    def city(self, text):
-        print(text)
+    def city(self):
+        return self.__state
+    
+    # @city.setter
+    # def city(self, text):
+    #     self.__city = text
         
     def __str__(self) -> str:
-        return f"{self.city()}"
+        return self.city
+    
+    def func(self, text):
+        self.city = text
+        
         
 inst = Ch()
+inst.func("Python is cool")
 print(inst)
