c519e06427c091e08a6021f243825e574c18d4a6
Huclark <vhuclark@gmail.com>
2024-02-22 01:42:41 +0000

    Fox: test cases fixed now

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 """ Module for testing file storage"""
+import datetime
+import json
+import os
 import unittest
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
@@ -9,8 +12,6 @@ from models.place import Place
 from models.review import Review
 from models.user import User
 from models.city import City
-from models import storage
-import os
 
 
 class test_fileStorage(unittest.TestCase):
@@ -21,7 +22,7 @@ class test_fileStorage(unittest.TestCase):
         self.test_storage = FileStorage()
         self.test_storage._FileStorage__file_path = "test_file.json"
         del_list = []
-        for key in self.test_storage._FileStorage__objects.keys():
+        for key in self.test_storage._FileStorage__objects:
             del_list.append(key)
         for key in del_list:
             del self.test_storage._FileStorage__objects[key]
@@ -54,7 +55,8 @@ class test_fileStorage(unittest.TestCase):
     def test_base_model_instantiation(self):
         """ File is not created on BaseModel save """
         new = BaseModel()
-        self.assertFalse(os.path.exists('file.json'))
+        self.test_storage.reload()
+        self.assertFalse(new in self.test_storage._FileStorage__objects)
 
     def test_empty(self):
         """ Data is saved to file """
@@ -105,7 +107,7 @@ class test_fileStorage(unittest.TestCase):
 
     def test_reload_empty(self):
         """ Load from an empty file """
-        with open('file.json', 'w') as f:
+        with open('test_file.json', 'w') as f:
             pass
         with self.assertRaises(ValueError):
             self.test_storage.reload()
@@ -169,3 +171,117 @@ class test_fileStorage(unittest.TestCase):
         from models.engine.file_storage import FileStorage
         print(type(self.test_storage))
         self.assertEqual(type(self.test_storage), FileStorage)
+
+    def test_reload_corrupted_file(self):
+        """Test if reload() does nothing if the JSON file is has an
+        invalid format
+        """
+        # Open a JSON file
+        with open("test_file.json", "w", encoding="utf-8") as file:
+            # Create an invalid JSON format in the JSON file
+            file.write("Trying to create an invalid JSON file")
+        try:
+            # Reload the content from the JSON file
+            self.test_storage.reload()
+        except json.JSONDecodeError:
+            pass
+        # Check if __objects is still empty
+        self.assertEqual(self.test_storage.all(), {})
+
+    def wrong_file_path(self):
+        """Test reload with wrong file path
+        """
+        # create a JSON file with save()
+        self.test_storage.save()
+        # Modify file path
+        self.test_storage._FileStorage__file_path = "new.json"
+        # Reload from a different file path
+        self.test_storage.reload()
+        # Check if __object is still empty
+        # since the file path was changed
+        self.assertEqual(self.test_storage.all(), {})
+
+    def test_special_characters(self):
+        """Test if save() can handle instances with attributes
+        containing special characters
+        """
+        # Create an instance with attributes containing special characters
+        timestamp = datetime.datetime.isoformat(datetime.datetime.now())
+        user = User(id="Dmi#%*2", created_at=timestamp,
+                    updated_at=timestamp, name="John&Doe")
+        # Call new() on user
+        self.test_storage.new(user)
+        # Save user onto the JSON file
+        self.test_storage.save()
+        # Reload and check if the instance is present
+        self.test_storage.reload()
+        self.assertIn("User.Dmi#%*2", self.test_storage.all())
+
+    def test_empty_instances(self):
+        """Test save() and reload() on empty instances to ensure it
+        does not raise any errors
+        """
+        # Create a User instance
+        user = User()
+        # Call new() on user
+        self.test_storage.new(user)
+        # Save user onto the JSON file
+        self.test_storage.save()
+        # Reload and check if the instance is present
+        self.test_storage.reload()
+        self.assertIn("User." + user.id, self.test_storage.all())
+
+    def test_new_args(self):
+        """Test new() method with more arguments than it takes
+        """
+        # Raise a TypeError
+        with self.assertRaises(TypeError):
+            self.test_storage.new(User(), 1)
+
+    def test_reload_args(self):
+        """Test new() method with invalid argument type
+        """
+        # Raise TypeError
+        with self.assertRaises(TypeError):
+            self.test_storage.reload(None)
+
+    def test_updated_attributes(self):
+        """Test save() and reload() method by modifying the attributes
+        of instances
+        """
+        # Create an instance
+        timestamp = datetime.datetime.isoformat(datetime.datetime.now())
+        user = User(id="1234", created_at=timestamp,
+                    updated_at=timestamp, first_name="John")
+        # Call new()
+        self.test_storage.new(user)
+        # Save the user instance
+        self.test_storage.save()
+        # Update the user attribute
+        user.first_name = "James"
+        # Save the change
+        self.test_storage.save()
+        # Load data from JSON file
+        self.test_storage.reload()
+        # Get the User instance from __objects
+        user_obj = self.test_storage.all()["User." + user.id]
+        # Check if the updated attribute is reflected
+        self.assertEqual(user_obj.first_name, "James")
+
+    def test_deleted_instance(self):
+        """Test reload() and save() methods after deleting an instance
+        """
+        # Create an instance
+        time = datetime.datetime.isoformat(datetime.datetime.now())
+        place = Place(id="12k34", created_at=time, updated_at=time,
+                      first_name="John")
+        # Call place()
+        self.test_storage.new(place)
+        # Save the place instance
+        self.test_storage.save()
+        # Delete the place instance and save()
+        del self.test_storage._FileStorage__objects["Place." + place.id]
+        self.test_storage.save()
+        # Reload and check if the removed instance is still absent
+        self.test_storage.reload()
+        self.assertNotIn("Place." + place.id, self.test_storage.all())
