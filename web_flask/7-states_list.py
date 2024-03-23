d8118f257a4cfc1c7d01752782774c68c6a56cff
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2024-03-23 20:49:45 +0100

    task 7

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,7 +2,7 @@
 """script that starts a Flask web application"""
 
 from flask import Flask, render_template
-from models import *
+from models.state import State
 from models import storage
 
 
@@ -13,7 +13,7 @@ app = Flask(__name__)
 def states_list():
     """display a HTML page with the list of all
     State objects present in DBStorage sorted by name (A->Z)"""
-    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
+    states = sorted(list(storage.all(State).values()), key=lambda x: x.name)
     return render_template("7-states_list.html", states=states)
 
 
