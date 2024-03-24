489d1d65a23b58a179390a32fa56a7388e2433a9
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2024-03-24 09:55:02 +0100

    update

--- web_flask/100-hbnb.py
+++ web_flask/100-hbnb.py
@@ -8,7 +8,7 @@ from models.state import State
 from models import storage
 
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static', static_folder='static')
 
 
 @app.route("/hbnb", strict_slashes=False)
